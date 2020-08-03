d882f81ba831b7abdfcae32277aeed3ee6708f52
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-08-03 11:20:39 +0900

    APIへの接続部分を関数化

--- js-weather/weather.js
+++ js-weather/weather.js
@@ -1,5 +1,23 @@
 'use strict';
 
+
+//APIのコールを関数化
+const REQUEST_AJAX = async (city_id) => {
+
+    const URL = `http://api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=4b5774e9f3d2a07b84f0f2f88e486224&lang=ja&units=metric`;
+    const JSON = await fetch(URL)
+        .then(res => {
+            return res.json();
+        }).catch(error => {
+            console.log('APIへのアクセスに失敗しました');
+            return null;
+        });
+    const WEATHER = JSON.weather[0].description;
+    const WEATHER_DESCRIPTION = document.getElementById('weather_description');
+    WEATHER_DESCRIPTION.textContent = `現在の天気は${WEATHER}です`;
+}
+
+
 //プルダウンから選んだ年の天気を表示する
 const SELECT_CITY = document.querySelector('.city');
 SELECT_CITY.addEventListener('change', (event) => {
